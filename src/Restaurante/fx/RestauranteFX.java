e89860743e85173ceba67ab0c40634956ea57904
jasonk78 <60988473+jasonk78@users.noreply.github.com>
2020-03-01 15:15:59 -0600

    Add files via upload

---
+++ src/Restaurante/fx/RestauranteFX.java
@@ -0,0 +1,42 @@
+
+package Restaurante.fx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Daniel
+ */
+public class RestauranteFX extends Application {
+    public static boolean isSplashLoaded = false;
+    static Stage stage;
+    
+    public static Stage getStage(){
+        return stage;
+    }
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        RestauranteFX.stage = stage;
+        Parent root = FXMLLoader.load(getClass().getResource("/Restaurante/fx/Menu/main.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.setTitle("Restaurante");
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+        
+    }
+    
+}
